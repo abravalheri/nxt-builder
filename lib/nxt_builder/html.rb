496519dea38cbc1731ccab7682fc57f3af5f5232
Anderson Bravalheri <andersonbravalheri@gmail.com>
2015-09-21 01:02:04 -0300

    Add measures without html attributes verification

--- lib/nxt_builder/html.rb
+++ lib/nxt_builder/html.rb
@@ -22,31 +22,31 @@ module NxtBuilder
       end
     end
 
-    def _tag(name, content_or_options = nil, options = {})
-      if content_or_options.kind_of?(Hash)
-        opt = content_or_options
-      else
-        opt = options
-      end
-
-      # "Flatten" 1-dept prefixed hash attrs
-      TAG_PREFIXES.each do |prefix|
-        if opt[prefix]
-          prefixed = opt.delete(prefix) || {}
-          prefixed.inject(opt) do |acc,v|
-            acc["#{prefix}-#{v[0].to_s.gsub('_', '-')}"] = v[1]; acc
-          end
-        end
-      end
-
-      # Canonicalize boolean attributes
-      opt.each do |k,v|
-        if BOOLEAN_ATTRIBUTES.include?(k.to_s)
-          v ? (opt[k] = k.to_s) : opt.delete(k)
-        end
-      end
-
-      super
-    end
+    # def _tag(name, content_or_options = nil, options = {})
+    #   if content_or_options.kind_of?(Hash)
+    #     opt = content_or_options
+    #   else
+    #     opt = options
+    #   end
+    #
+    #   # "Flatten" 1-dept prefixed hash attrs
+    #   TAG_PREFIXES.each do |prefix|
+    #     if opt[prefix]
+    #       prefixed = opt.delete(prefix) || {}
+    #       prefixed.inject(opt) do |acc,v|
+    #         acc["#{prefix}-#{v[0].to_s.gsub('_', '-')}"] = v[1]; acc
+    #       end
+    #     end
+    #   end
+    #
+    #   # Canonicalize boolean attributes
+    #   opt.each do |k,v|
+    #     if BOOLEAN_ATTRIBUTES.include?(k.to_s)
+    #       v ? (opt[k] = k.to_s) : opt.delete(k)
+    #     end
+    #   end
+    #
+    #   super
+    # end
   end
 end
--- spec/nxt_builder/html_spec.rb
+++ spec/nxt_builder/html_spec.rb
@@ -3,31 +3,31 @@ require 'spec_helper'
 describe NxtBuilder::HTML do
   it { is_expected.to respond_to(*NxtBuilder::HTML::TAGS) }
 
-  describe "#_tag" do
-    context "when called with a data attribute hash" do
-      it "should flatten" do
-        expect(subject._tag(:p, data: { a: 1 }).to_s).to start_with('<p data-a="')
-      end
-    end
-
-    context "when called with a boolean attribute" do
-      context "when attribute is false" do
-        let!(:result) { subject._tag(:input, disabled: false).to_s }
-
-        it "should remove attribute" do
-          expect(result).not_to include('disabled')
-        end
-      end
-
-      context "when attribute is true" do
-        let!(:result) { subject._tag(:input, disabled: true).to_s }
-
-        it "should include attribute" do
-          expect(result).to include('disabled')
-        end
-      end
-    end
-  end
+  # describe "#_tag" do
+  #   context "when called with a data attribute hash" do
+  #     it "should flatten" do
+  #       expect(subject._tag(:p, data: { a: 1 }).to_s).to start_with('<p data-a="')
+  #     end
+  #   end
+  #
+  #   context "when called with a boolean attribute" do
+  #     context "when attribute is false" do
+  #       let!(:result) { subject._tag(:input, disabled: false).to_s }
+  #
+  #       it "should remove attribute" do
+  #         expect(result).not_to include('disabled')
+  #       end
+  #     end
+  #
+  #     context "when attribute is true" do
+  #       let!(:result) { subject._tag(:input, disabled: true).to_s }
+  #
+  #       it "should include attribute" do
+  #         expect(result).to include('disabled')
+  #       end
+  #     end
+  #   end
+  # end
 
   describe "#render!" do
     it "should output idented nodes" do
